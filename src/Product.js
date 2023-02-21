d11e549dace8b0883af5f85b1a177b3faafa4981
devp1447@gmail.com <devp1447@gmail.com>
2023-02-21 16:00:16 +0530

    forth-commit

---
+++ src/Product.js
@@ -0,0 +1,24 @@
+export default [
+    {
+        "id": "Aksharabhyasam",
+        "title":"Aksharabhyasam",
+        "imgurl": "https://cdn.image.harivara.com/Tamil/Ceremonies/Aksharabhyasam-Harivara-Tamil.jpg",
+
+    },
+    {
+        "id": "Annaprasana",
+        "title":"Annaprasana",
+        "imgurl": "https://cdn.image.harivara.com/Tamil/Ceremonies/Annaprasana-Harivara-Tamil.jpg",
+
+    },
+    {
+        "id": "Fix Your Muhurth Dates",
+        "title":"mariage",
+        "imgurl": "https://cdn.image.harivara.com/Kannada/Poojas/Punyahavachanam-Harivara-Kannada.jpg",
+
+    },
+    // {
+    //     "id": "Karna Vedha (Ear Piercing)",
+    //     "imgurl": "https://cdn.image.harivara.com/Tamil/Ceremonies/Karna-Vedha-Harivara-Tamil.jpg",
+    // }
+]
--- src/section/FeaturedPuja.js
+++ src/section/FeaturedPuja.js
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import "../csscomponent/Featuredpujas.css"
+import Product from "../Product"
+console.log(Product)
 const FeaturedPuja = () => {
+
   return (
-    <div>FeaturedPuja</div>
+
+    <div className='container'>
+      <div className='row'>
+        <div className='col-lg-12 featuredcol1'>
+          <h1>Featured Pujas</h1>
+          <p>These are most Popular Pujas and  Homas performed  by our Pandit</p>
+        </div>
+      </div>
+      <div className='row featuredrowstyle'>
+        {Product.map((data, key) => {
+          return (
+            <div className='col-lg-3 featuredcolStyle' key={data.id}>
+              <img src={data.imgurl} alt={data.id} />
+              <div>{data.title}</div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
